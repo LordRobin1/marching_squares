f7636decbade65776dec0d3131a69c4d4f26d98d
LordRobin1 <geb611018t@gmail.com>
2023-07-08 11:28:49 +0200

    main + cargo clean up

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,7 @@
 #![allow(unused)]
 #![allow(dead_code)]
 
-// use error_iter::ErrorIter as _;
-use log::{debug, error};
 use metaballs::run;
-use pixel_lib::{ColorMode::*, *};
-use std::sync::{Arc, Mutex};
-use std::thread;
-use std::time::{Duration, Instant};
-use winit::dpi::PhysicalSize;
-use winit::event::{Event, WindowEvent, WindowEvent::CursorMoved};
-use winit::event_loop::{ControlFlow, EventLoop};
-use winit::window::WindowBuilder;
 
 fn main() {
     pollster::block_on(run());
