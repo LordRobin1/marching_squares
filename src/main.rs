f566027180cfa8087cd0fbea114a90327a623e5b
Robin Gebhardt <geb611018t@gmail.com>
2023-08-15 23:07:38 +0200

    completed marching squares logic, implicit function macro - Rasterization missing - Macro allows to specify any implicit function and immediately provide the needed args, such that the square only needs to call it with the current point as arg.

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,9 @@
-#![allow(unused)]
-#![allow(dead_code)]
-
-use pixel_lib::{Color, ColorMode::*, *};
+use pixel_lib::{Color, *};
 use softbuffer::GraphicsContext;
-use std::sync::{Arc, Mutex};
-use std::thread;
-use std::time::{Duration, Instant};
+use std::time::Instant;
 use winit::dpi::PhysicalSize;
 use winit::event::{ElementState, KeyboardInput, VirtualKeyCode};
-use winit::event::{Event, WindowEvent, WindowEvent::CursorMoved};
+use winit::event::{Event, WindowEvent};
 use winit::event_loop::{ControlFlow, EventLoop};
 use winit::window::WindowBuilder;
 
@@ -20,13 +15,10 @@ use crate::square::*;
 fn main() {
     let event_loop = EventLoop::new();
     let window = WindowBuilder::new().build(&event_loop).unwrap();
-    let mut graphics_context = unsafe { GraphicsContext::new(&window, &window) }.unwrap();
-    let mut fps: u128 = 0;
-    let mut start = Instant::now();
+    let graphics_context = unsafe { GraphicsContext::new(&window, &window) }.unwrap();
     let mut delta_time = Default::default();
-    let mut last_len = 0;
 
-    let mut cursor = Point { x: 0., y: 0. };
+    let mut cursor = Point::origin();
 
     let mut state = State::new(&window.inner_size(), graphics_context);
 
@@ -35,9 +27,7 @@ fn main() {
 
         match event {
             Event::MainEventsCleared => {
-                start = Instant::now();
-
-                let size = window.inner_size();
+                let start = Instant::now();
 
                 state.render();
                 state.update(&cursor, delta_time);
@@ -46,7 +36,7 @@ fn main() {
                 let time = start.elapsed();
                 delta_time = time.as_micros() as f32 / 1_000_000.0;
                 print!("\r");
-                fps = 1_000_000 / time.as_micros();
+                let fps = 1_000_000 / time.as_micros();
                 print!(
                     "FPS: {}, Cursor: {}, {}{}",
                     fps,
@@ -100,40 +90,19 @@ struct State {
 
 impl State {
     fn new(size: &PhysicalSize<u32>, context: GraphicsContext) -> Self {
-        let mid = Point {
-            x: size.width as f32 / 2.,
-            y: size.height as f32 / 2.,
-        };
-        let p1 = Point {
-            x: mid.x + 70.,
-            y: mid.y,
-        };
-        let p2 = Point {
-            x: mid.x - 70.,
-            y: mid.y,
-        };
+        let mid = Point::new(size.width / 2, size.height / 2);
+        let p1 = Point::new(mid.x as u32 + 70, mid.y as u32);
+        let p2 = Point::new(mid.x as u32 - 70, mid.y as u32);
+
         let radius = 0.1 * (size.width.pow(2) as f32 + size.height.pow(2) as f32).sqrt();
-        let red = Color {
-            r: 1.,
-            g: 0.,
-            b: 0.,
-            a: 1.,
-        };
-        let green = Color {
-            r: 0.,
-            g: 1.,
-            b: 0.,
-            a: 1.,
-        };
-        let blue = Color {
-            r: 0.,
-            g: 0.,
-            b: 1.,
-            a: 1.,
-        };
-        let mut balls = vec![
+
+        let red = Color::new(1., 0., 0., 1.);
+        let green = Color::new(0., 1., 0., 1.);
+        let blue = Color::new(0., 0., 1., 1.);
+
+        let balls = vec![
             Ball {
-                position: Point { x: 0., y: 0. },
+                position: Point::origin(),
                 radius,
                 velocity: Point { x: 75., y: -60. },
                 color: red,
@@ -157,41 +126,19 @@ impl State {
             context,
             size: (size.width as f32, size.height as f32),
             grid_res: 100,
-            cursor: Point { x: 0., y: 0. },
+            cursor: Point::origin(),
             update: true,
         }
     }
 
     fn render(&mut self) {
-        let (width, height) = self.size;
         self.marching_squares();
         self.context
             .set_buffer(&self.buffer, self.size.0 as u16, self.size.1 as u16);
         self.buffer = vec![0; (self.size.0 * self.size.1) as usize]
     }
 
-    fn shade(&mut self) {
-        let (width, height) = self.size;
-        let mut pxl: Pixel;
-
-        for y in 0..height as i32 {
-            for x in 0..width as i32 {
-                pxl = Pixel {
-                    pos: Point {
-                        x: x as f32,
-                        y: y as f32,
-                    },
-                    color: Color {
-                        ..Default::default()
-                    },
-                };
-                self.metaball_shader(&mut pxl, Overlay);
-                self.buffer[(y as f32 * width + x as f32) as usize] = pxl.color.as_u32();
-            }
-        }
-    }
-
-    fn update(&mut self, cursor: &Point, delta_time: f32) {
+    fn update(&mut self, _cursor: &Point, delta_time: f32) {
         if !self.update {
             return;
         }
@@ -211,15 +158,22 @@ impl State {
     fn marching_squares(&mut self) {
         let dimension = self.size.0 as u32 / self.grid_res;
         let (mut x, mut y) = (0u32, 0u32);
+
+        let compute_influence = |p: Point, balls: &[Ball]| -> f32 {
+            let mut sum = 0.;
+            for ball in balls {
+                let influence = (ball.radius.powi(2) / p.sq_distance(&ball.position)).clamp(0., 2.);
+                sum += influence.clamp(0., 1.);
+            }
+            sum
+        };
+
+        let implicit_fn = implicit_fn!(compute_influence, &self.balls);
+
         while (0..self.size.1 as u32).contains(&y) {
             while (0..self.size.0 as u32).contains(&x) {
-                let mut square = Square {
-                    origin: Point {
-                        x: x as f32,
-                        y: y as f32,
-                    },
-                    dimension: dimension as f32,
-                };
+                let mut square =
+                    Square::new(Point::new(x, y), dimension as f32, vec![], &implicit_fn);
                 square.march(
                     &mut self.buffer,
                     &self.balls,
@@ -229,42 +183,7 @@ impl State {
                 x += dimension;
             }
             y += dimension;
-            x = 0;
+            x ^= x;
         }
     }
-
-    /// pixel based metaballs (not optimized because i'm too dumb for that polynomial stuff)
-    fn metaball_shader(&mut self, pxl: &mut Pixel, col_mode: ColorMode) {
-        let mut sum = 0.;
-        for ball in self.balls.as_mut_slice() {
-            let influence =
-                { (ball.radius.powi(2) / pxl.pos.sq_distance(&ball.position)).clamp(0., 2.) };
-            sum += influence.clamp(0., 1.);
-            pxl.color.add(&ball.color.mult(influence));
-        }
-        if sum < 1.
-        /*|| sum > 1.03*/
-        {
-            pxl.color = Color {
-                r: 0.,
-                g: 0.,
-                b: 0.,
-                a: 0.,
-            };
-        }
-    }
-}
-
-/// returns a value between 0. and 1.
-fn step(value: f32, edge: f32) -> f32 {
-    match value < edge {
-        true => 1.,
-        _ => 0.,
-    }
-}
-
-/// returns a smooth value between 0. and 1.
-fn smooth_step(value: f32, edge_0: f32, edge_1: f32) -> f32 {
-    let x = ((value - edge_0) / (edge_1 - edge_0)).clamp(0., 1.);
-    x * x * (3. - 2. * x)
 }
--- src/square.rs
+++ src/square.rs
@@ -1,11 +1,31 @@
-use std::ops::Add;
-
 use crate::balls::*;
 use pixel_lib::*;
 
-pub struct Square {
-    pub origin: Point,
-    pub dimension: f32,
+#[macro_export]
+/// # Creates a closure which takes a `Point` and is then used to compute the implicit function
+/// Pass a closure to this macro which takes a `Point` as the 1st argument and everything else that it needs after that
+///
+/// ---
+/// Example:
+/// ```
+/// let func = |p: Point, constant: f32| -> f32 {
+///     p.x * constant
+/// };
+/// let implicit_func = implicit_fn!(func, 3.14);
+macro_rules! implicit_fn {
+    ($func: expr, $($arg: expr),*) => {
+        |p: Point| -> f32 {
+            $func(p, $($arg),*)
+        }
+    };
+}
+
+/// Squares for the marching squares algorithm
+pub struct Square<'a> {
+    origin: Point,
+    dimension: f32,
+    weights: Vec<f32>,
+    implicit_fn: &'a dyn Fn(Point) -> f32,
 }
 
 struct WeightedPoint {
@@ -13,128 +33,155 @@ struct WeightedPoint {
     y: f32,
     weight: f32,
 }
-
-/// T: top, B: bottom, l: left, r: right
-/// this will be removed later since it causes the need to match two times
-enum Case {
-    Full,
-    Empty,
-    Tl,
-    Tr,
-    Bl,
-    Br,
-    TlBl,
-    TrBr,
-    TlTr,
-    BlBr,
-    TlBr,
-    TrBl,
-    TlTrBl,
-    TlTrBr,
-    TrBlBr,
-    TlBrBl,
+impl WeightedPoint {
+    pub fn new(x: f32, y: f32, weight: f32) -> Self {
+        Self { x, y, weight }
+    }
 }
 
-impl Square {
-    pub fn march(&mut self, buffer: &mut [u32], balls: &[Ball], width: u32, height: u32) {
-        let formula = |p: Point| -> f32 {
-            let mut sum = 0.;
-            for ball in balls {
-                let influence = (ball.radius.powi(2) / p.sq_distance(&ball.position)).clamp(0., 2.);
-                sum += influence.clamp(0., 1.);
-            }
-            sum
-        };
-        let tl = formula(self.origin);
-        let tr = formula(self.origin.add(&Point {
-            x: self.dimension,
-            y: 0.,
-        }));
-        let bl = formula(self.origin.add(&Point {
-            x: 0.,
-            y: self.dimension,
-        }));
-        let br = formula(self.origin.add(&Point {
-            x: self.dimension,
-            y: self.dimension,
-        }));
-        // actual code not written yet
-        // visual debugging purposes only:
-        let case = self.shade(tl, tr, bl, br);
-        if let Case::Full = case {
-            let y_range = if self.origin.y + self.dimension > height as f32 {
-                self.origin.y as u32..height
-            } else {
-                self.origin.y as u32..(self.origin.y + self.dimension) as u32
-            };
-
-            for mut y in y_range {
-                let x_range = if self.origin.x + self.dimension > width as f32 {
-                    self.origin.x as u32..(width)
-                } else {
-                    self.origin.x as u32..(self.origin.x + self.dimension) as u32
-                };
-                for mut x in x_range {
-                    let i = (x + y * width) as usize;
-                    buffer[i] = Color {
-                        r: 0.,
-                        g: 0.8,
-                        b: 0.0,
-                        a: 1.,
-                    }
-                    .as_u32();
-                }
-            }
+impl<'a> Square<'a> {
+    pub fn new(
+        origin: Point,
+        dimension: f32,
+        weights: Vec<f32>,
+        implicit_fn: &'a dyn Fn(Point) -> f32,
+    ) -> Self {
+        Self {
+            origin,
+            dimension,
+            weights,
+            implicit_fn,
         }
     }
 
-    fn shade(&self, tl: f32, tr: f32, bl: f32, br: f32) -> Case {
-        let tl_point = WeightedPoint {
-            x: self.origin.x,
-            y: self.origin.y,
-            weight: tl,
-        };
-        let tr_point = WeightedPoint {
-            x: self.origin.x + self.dimension,
-            y: self.origin.y,
-            weight: tr,
-        };
-        let bl_point = WeightedPoint {
-            x: self.origin.x,
-            y: self.origin.y + self.dimension,
-            weight: bl,
-        };
-        let br_point = WeightedPoint {
-            x: self.origin.x + self.dimension,
-            y: self.origin.y + self.dimension,
-            weight: br,
-        };
-        if tl > 1. || tr > 1. || bl > 1. || br > 1. {
-            return Case::Full;
-        } else {
-            return Case::Empty;
-        }
-        match (tl > 1., tr > 1., bl > 1., br > 1.) {
-            (true, true, true, true) => Case::Full,
-            (false, false, false, false) => Case::Empty,
+    pub fn march(&mut self, buffer: &mut [u32], balls: &[Ball], width: u32, height: u32) {
+        let impl_fun = self.implicit_fn;
+        let tl = impl_fun(self.origin);
+        let tr = impl_fun(self.origin.add(&Point {
+            x: self.dimension,
+            y: 0.,
+        }));
+        let bl = impl_fun(self.origin.add(&Point {
+            x: 0.,
+            y: self.dimension,
+        }));
+        let br = impl_fun(self.origin.add(&Point {
+            x: self.dimension,
+            y: self.dimension,
+        }));
+
+        self.weights = vec![tl, tr, bl, br];
+
+        self.shade(width, height, buffer);
+    }
+
+    /// Calculates the contours intersection points with the square
+    /// and calls `rasterize_line` if it intersects
+    fn shade(&self, width: u32, height: u32, buffer: &mut [u32]) {
+        let hori_bound = self.origin.x + self.dimension;
+        let vert_bound = self.origin.y + self.dimension;
+        let tl = WeightedPoint::new(self.origin.x, self.origin.y, self.weights[0]);
+        let tr = WeightedPoint::new(hori_bound, self.origin.y, self.weights[1]);
+        let bl = WeightedPoint::new(self.origin.x, vert_bound, self.weights[2]);
+        let br = WeightedPoint::new(hori_bound, vert_bound, self.weights[3]);
+
+        // some of the cases can be combined
+        match (
+            self.weights[0] > 1.,
+            self.weights[1] > 1.,
+            self.weights[2] > 1.,
+            self.weights[3] > 1.,
+        ) {
+            (true, true, true, true) => (),
+            (false, false, false, false) => (),
             (true, false, false, false) => {
-                // Tl inside, rest outside
-                self.compute_intersection((&tl_point, &tr_point), (&tl_point, &bl_point));
-                Case::Tl
+                // X O
+                // O O
+                let (p1, p2) = self.compute_intersection((&tl, &tr), (&tl, &bl));
+                self.rasterize_line(&p1, &p2, buffer, width, height);
             }
-            (false, true, false, false) => Case::Tr,
-            (false, false, true, false) => Case::Bl,
-            (false, false, false, true) => Case::Br,
-            (true, false, true, false) => Case::TlBl,
-            (false, true, false, true) => Case::TrBr,
-            (true, true, false, false) => Case::TlTr,
-            (false, false, true, true) => Case::BlBr,
-            (true, false, false, true) => Case::TlBr,
-            (false, true, true, false) => Case::TrBl,
-            (true, true, true, false) => Case::TlTrBl,
-            (true, true, false, true) => Case::TlTrBr,
-            (true, false, true, true) => Case::TlBrBl,
-            (false, true, true, true) => Case::TrBlBr,
+            (false, true, false, false) => {
+                // O X
+                // O O
+                let (p1, p2) = self.compute_intersection((&tr, &tl), (&tr, &br));
+                self.rasterize_line(&p1, &p2, buffer, width, height);
+            }
+            (false, false, true, false) => {
+                // O O
+                // X O
+                let (p1, p2) = self.compute_intersection((&bl, &tl), (&bl, &br));
+                self.rasterize_line(&p1, &p2, buffer, width, height);
+            }
+            (false, false, false, true) => {
+                // O O
+                // O X
+                let (p1, p2) = self.compute_intersection((&br, &bl), (&br, &tr));
+                self.rasterize_line(&p1, &p2, buffer, width, height);
+            }
+            (true, false, true, false) => {
+                // X O    O X
+                // X O    O X
+                let (p1, p2) = self.compute_intersection((&tl, &tr), (&bl, &br));
+                self.rasterize_line(&p1, &p2, buffer, width, height);
+            }
+            (false, true, false, true) => {
+                // O X
+                // O X
+                let (p1, p2) = self.compute_intersection((&tl, &tr), (&bl, &br));
+                self.rasterize_line(&p1, &p2, buffer, width, height);
+            }
+            (true, true, false, false) => {
+                // X X    O O
+                // O O    X X
+                let (p1, p2) = self.compute_intersection((&tl, &bl), (&tr, &br));
+                self.rasterize_line(&p1, &p2, buffer, width, height);
+            }
+            (false, false, true, true) => {
+                // O O
+                // X X
+                let (p1, p2) = self.compute_intersection((&tl, &bl), (&tr, &br));
+                self.rasterize_line(&p1, &p2, buffer, width, height);
+            }
+            (true, false, false, true) => {
+                // X O
+                // O X
+                let (p1, p2) = self.compute_intersection((&tl, &tr), (&tl, &bl));
+                self.rasterize_line(&p1, &p2, buffer, width, height);
+                let (p3, p4) = self.compute_intersection((&br, &bl), (&bl, &tl));
+                self.rasterize_line(&p3, &p4, buffer, width, height);
+            }
+            (false, true, true, false) => {
+                // O X
+                // X O
+                let (p1, p2) = self.compute_intersection((&tl, &tr), (&tl, &bl));
+                self.rasterize_line(&p1, &p2, buffer, width, height);
+                let (p3, p4) = self.compute_intersection((&br, &bl), (&bl, &tl));
+                self.rasterize_line(&p3, &p4, buffer, width, height);
+            }
+            (true, true, true, false) => {
+                // X X
+                // X O
+                let (p1, p2) = self.compute_intersection((&bl, &br), (&tr, &br));
+                self.rasterize_line(&p1, &p2, buffer, width, height);
+            }
+            (true, true, false, true) => {
+                // X X
+                // O X
+                let (p1, p2) = self.compute_intersection((&bl, &br), (&tl, &bl));
+                self.rasterize_line(&p1, &p2, buffer, width, height);
+            }
+            (true, false, true, true) => {
+                // X O
+                // X X
+                let (p1, p2) = self.compute_intersection((&tl, &tr), (&br, &tr));
+                self.rasterize_line(&p1, &p2, buffer, width, height);
+            }
+            (false, true, true, true) => {
+                // O X
+                // X X
+                let (p1, p2) = self.compute_intersection((&tl, &tr), (&tl, &bl));
+                self.rasterize_line(&p1, &p2, buffer, width, height);
+            }
         }
     }
 
@@ -164,5 +211,31 @@ impl Square {
             intersect
         };
         (compute(pair1), compute(pair2))
+    }
+
+    fn rasterize_line(&self, p1: &Point, p2: &Point, buffer: &mut [u32], width: u32, height: u32) {
+        let y_range = if self.origin.y + self.dimension > height as f32 {
+            self.origin.y as u32..height
+        } else {
+            self.origin.y as u32..(self.origin.y + self.dimension) as u32
+        };
+
+        for y in y_range {
+            let x_range = if self.origin.x + self.dimension > width as f32 {
+                self.origin.x as u32..(width)
+            } else {
+                self.origin.x as u32..(self.origin.x + self.dimension) as u32
+            };
+            for x in x_range {
+                let i = (x + y * width) as usize;
+                buffer[i] = Color {
+                    r: 0.,
+                    g: 0.8,
+                    b: 0.0,
+                    a: 1.,
+                }
+                .as_u32();
+            }
+        }
     }
 }
